cdd110c2d51c915536a5ee7e299b8a3cee814ab6
Nico Lopez <nnicolopez@gmail.com>
2019-07-21 16:27:34 -0300

    Put some mock code in recipe list and detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test',
+      'This is a recipe',
+      'https://www.hogarmania.com/archivos/201502/1580-coca-de-puerro-y-mejillones-764-xl-668x400x80xX.jpg'
+    ),
+    new Recipe(
+      'Test',
+      'This is a recipe',
+      'https://www.hogarmania.com/archivos/201502/1580-coca-de-puerro-y-mejillones-764-xl-668x400x80xX.jpg'
+    )
+  ];
 
   constructor() { }
 
